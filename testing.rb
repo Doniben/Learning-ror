efd78dce6afa98a34afb1c626ab64a223c4c23ec
doniben <doniben@esperanto.co>
2022-02-27 16:29:36 -0500

    testing with wimple calculator

---
+++ testing.rb
@@ -0,0 +1,25 @@
+class Calculator
+    def sum(a, b)
+        a + b
+    end
+
+    def substract(a, b)
+        a - b
+    end
+end
+
+calc = Calculator.new
+test_sum = {
+    [1, 2] => 3,
+    [2, 2] => 4,
+    [3, 2] => 5,
+    [4, 2] => 6,
+}
+
+test_sum.each do |input, expected|
+    if !(calc.sum(input[0], input[1]) == expected)
+        raise "Test failed for input #{input}. Expected result #{expected} but was #{calc.sum(input[0], input[1])}"
+    else
+        puts "Test passed for input #{input} as expected result #{expected}"
+    end
+end
